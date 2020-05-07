cc27f034be66c22ff792df5e2ad03637dcfb7e9a
ANURAG THAKUR <davanu100@gmail.com>
2020-05-07 16:26:03 +0530

    Adding basic routes and controllers logic

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
-const express = require("express") ;
-const path = require("path") ;
+const path = require('path') ;
+
+const express = require('express') ;
+
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const MONGODB_URI = 'mongodb+srv://anurag:1ukBCbEqTxMCe2gz@cluster0-p7ghp.mongodb.net/test?retryWrites=true&w=majority';
+
 const app = express() ;
 
-app.set("view engine","ejs");
-app.set("views","views") ;
-app.use(express.static(path.join(__dirname,"public"))) ;
-app.use((re,res,next) => {
-    res.render(path.join(__dirname,".","views","admin","add-product.ejs"))
-})
+//body-parser  related code - anurag
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.listen(3000) ;
+// setting viw engine to ejs - pj
+app.set('view engine','ejs');
+app.set('views','views') ;
+
+//routesRegistered - anurag
+
+// const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+// const authRoutes = require('./routes/auth');
+
+//work - pj
+app.use(express.static(path.join(__dirname,"public"))) ;
+// app.use((re,res,next) => {
+//     res.render(path.join(__dirname,".","views","admin","add-product.ejs"))
+// });
+
+//RoutesCalled - anurag
+// app.use(adminRoutes);
+app.use(shopRoutes);
+// app.use(authRoutes);
+
+//mongooseConnection - anurag
+// mongoose
+// .connect(MONGODB_URI)
+// .then(result => {
+    app.listen(3000);
+// })
+// .catch(err => {
+//     console.log(err);
+// });
---
+++ routes/shop.js
@@ -0,0 +1,94 @@
+const path = require('path');
+
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
+const router = express.Router();
+
+//index-section - anurag
+router.get('/',shopController.getHome);
+
+
+// //men-section - anurag
+// router.get('/shop/men',shopController.getProductsShopMen);
+
+//     //topwear
+//     router.get('/men-topwear',shopController.getProductsMenTopWear);
+//     router.get('/men-tshirts',shopController.getProductsMenTshirts);
+//     router.get('/men-shirts',shopController.getProductsMenShirts);
+//     router.get('/men-sweaters',shopController.getProductsMenSweaters);
+//     router.get('/men-jackets',shopController.getProductsMenJackets);
+//     router.get('/men-sweatshirts',shopController.getProductsMenSweatshirts);
+
+//     //bottomwear
+//     router.get('/men-bottomwear',shopController.getProductsMenBottomWear);
+//     router.get('/men-jeans',shopController.getProductsMenJeans);
+//     router.get('/men-trousers',shopController.getProductsMenTrousers);
+//     router.get('/men-shorts',shopController.getProductsMenShorts);
+
+//     //footwear
+//     router.get('/men-footwear',shopController.getProductsMenFootWear);
+//     router.get('/men-casual-shoes',shopController.getProductsMenCasualShoes);
+//     router.get('/men-formals',shopController.getProductsMenFormals);
+//     router.get('/men-sports',shopController.getProductsMenSports);
+
+
+// //women-section - anurag
+// router.get('/shop/women',shopController.getProductsShopWomen);
+
+//     //westernwear
+//     router.get('/women-western',shopController.getProductsWomenWesternWear);
+//     router.get('/women-tops',shopController.getProductsWomenTops);
+//     router.get('/women-jeans',shopController.getProductsWomenJeans);
+//     router.get('/women-jackets-coats',shopController.getProductsWomenJacketsCoats);
+//     router.get('/women-shrugs',shopController.getProductsWomenShrugs);
+
+//     //indianwear
+//     router.get('/women-indian',shopController.getProductsWomenIndianWear);
+//     router.get('/women-kurtis',shopController.getProductsWomenKurtis);
+//     router.get('/women-lehengas',shopController.getProductsWomenLehengas);
+//     router.get('/women-sarees',shopController.getProductsWomenSarees);
+
+//     //footwear
+//     router.get('/women-footwear',shopController.getProductsWomenFootWear);    
+//     router.get('/women-boots',shopController.getProductsWomenBoots);    
+//     router.get('/women-flats',shopController.getProductsWomenFlats);    
+//     router.get('/women-heels',shopController.getProductsWomenHeels);    
+//     router.get('/women-casuals',shopController.getProductsWomenCasuals);    
+
+
+// //Kids section
+// router.get('/shop/kids',shopController.getProductsShopKids);
+
+//     //Boys-clothing
+//     router.get('/boys-clothing',shopController.getProductsBoysClothing);
+//     router.get('/boys-tshirts',shopController.getProductsBoysTshirts);
+//     router.get('/boys-shirts',shopController.getProductsBoysShirts);
+//     router.get('/boys-shorts',shopController.getProductsBoysShorts);
+//     router.get('/boys-jeans',shopController.getProductsBoysJeans);
+//     router.get('/boys-jackets',shopController.getProductsBoysJackets);
+
+//     //Boys-footwear
+//     router.get('/boys-footwear',shopController.getProductsBoysFootWear);
+//     router.get('/boys-casuals',shopController.getProductsBoysCasuals);
+//     router.get('/boys-sports',shopController.getProductsBoysSports);
+//     router.get('/boys-sandals',shopController.getProductsBoysSandals);
+
+//     //Girls-clothing
+//     router.get('/girls-clothing',shopController.getProductsGirlsClothing);    
+//     router.get('/girls-tops',shopController.getProductsGirlsTops);    
+//     router.get('/girls-tshirts',shopController.getProductsGirlsTshirts);    
+//     router.get('/girls-jeans',shopController.getProductsGirlsJeans);    
+//     router.get('/girls-jackets',shopController.getProductsGirlsJackets);    
+//     router.get('/girls-capris',shopController.getProductsGirlsCapris); 
+
+//     //Girls-footwear
+//     router.get('/girls-footwear',shopController.getProductsGirlsFootwear); 
+//     router.get('/girls-casuals',shopController.getProductsGirlsCasuals); 
+//     router.get('/girls-flip-flops',shopController.getProductsGirlsFlipFlops); 
+//     router.get('/girls-heels',shopController.getProductsGirlsHeels); 
+//     router.get('/girls-flats',shopController.getProductsGirlsFlats);
+
+
+module.exports = router;
